386a6079963f0dc26e2d66ca26c1f7422dd79d01
ArtyomAndr <125839403+ArtyomAndr@users.noreply.github.com>
2023-10-04 23:55:11 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module goStrCalc
+
+go 1.21
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import ("fmt"
+	"os"
+	"bufio"
+	"unicode"
+)
+func main() {
+	var text, operator string
+
+	scanner := bufio.NewReader(os.Stdin)
+
+	text, _ := bufio.NewReader(os.Stdin)
+
+	rs := rune[](text)
+
+
+	if unicode.IsLetter(rs[i])
+
+
+}
